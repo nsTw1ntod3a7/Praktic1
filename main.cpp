e80279f04cb7119344c80f06e7be802071ce2f2f
nsTw1ntod3a7 <credulous2005@mail.ru>
2025-10-13 11:22:19 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0,"");
+
+	int a_1 = 1; cout << "a_1 = " << a_1 << " | тип: int, размер: 4 б." << endl;
+	int a_2 = 790; cout << "a_2 = " << a_2 << " | тип: int, размер: 4 б." << endl;
+	int a_3 = 30765; cout << "a_3 = " << a_3 << " | тип: int, размер: 4 б." << endl;
+	int a_4 = 42; cout << "a_4 = " << a_4 << " | тип: int, размер: 4 б." << endl;
+	int a_5 = 52; cout << "a_5 = " << a_5 << " | тип: int, размер: 4 б." << endl;
+
+
+	short e_1 = 99858; cout << "e_1 = " << e_1 << " | тип: short, размер: 2 б." << endl;
+	short e_2 = 12783; cout << "e_2 = " << e_2 << " | тип: short, размер: 2 б." << endl;
+	short e_3 = 52244; cout << "e_3 = " << e_3 << " | тип: short, размер: 2 б." << endl;
+	short e_4 = 74856; cout << "e_4 = " << e_4 << " | тип: short, размер: 2 б." << endl;
+	short e_5 = 11000; cout << "e_5 = " << e_5 << " | тип: short, размер: 2 б." << endl;
+
+
+
+	float b_1 = 3.1415; cout << "b_1 = " << b_1 << " | тип float, размер 4 б." << endl;
+	float b_2 = 21.678; cout << "b_2 = " << b_2 << " | тип float, размер 4 б." << endl;
+	float b_3 = 0.78; cout << "b_3 = " << b_3 << " | тип float, размер 4 б." << endl;
+	float b_4 = 0.46; cout << "b_4 = " << b_4 << " | тип float, размер 4 б." << endl;
+	float b_5 = 7.45; cout << "b_5 = " << b_5 << " | тип float, размер 4 б." << endl;
+
+	double w_1 = 1.72; cout << "w_1 = " << w_1 << " | тип double, размер 8 б." << endl;
+	double w_2 = 0.457; cout << "w_2  = " << w_2 << " | тип double, размер 8 б." << endl;
+	double w_3 = .65454; cout << "w_3 = " << w_3 << " | тип double, размер 8 б." << endl;
+	double w_4 = 532.435; cout << "w_4 = " << w_4 << " | тип double, размер 8 б." << endl;
+	double w_5 = 2.8054; cout << "w_5 = " << w_5 << " | тип double, размер 8 б." << endl;
+
+	char c_1 = 'a'; cout << "c_1 = " << c_1 << " | тип char, размер 1 б." << endl;
+	char c_2 = 'b'; cout << "c_1 = " << c_2 << " | тип char, размер 1 б." << endl;
+	char c_3 = '?'; cout << "c_1 = " << c_3 << " | тип char, размер 1 б." << endl;
+	char c_4 = '+'; cout << "c_1 = " << c_4 << " | тип char, размер 1 б." << endl;
+	char c_5 = '$'; cout << "c_1 = " << c_5 << " | тип char, размер 1 б." << endl;
+		
+
+	bool d_1 = false; cout << "d_1 = 10>12 " << d_1 << " | тип bool, размер 1 б." << endl;
+	bool d_2= true; cout << "d_2 = 7<999 " << d_2 << " | тип bool, размер 1 б." << endl;
+	bool d_3 = false; cout << "d_3 = 10>12 " << d_3 << " | тип bool, размер 1 б." << endl;
+	bool d_4 = true; cout << "d_4 = 7>1 " << d_4 << " | тип bool, размер 1 б." << endl;
+	bool d_5 = false; cout << "d_5 = 1>115 " << d_5 << " | тип bool, размер 1 б." << endl;
+		
+	string s_1 = "Мяу"; cout << "s_1 = " << s_1 << " | тип string, размер 24-32 б." << endl;
+	string s_2 = "pupsik"; cout << "s_1 = " << s_1 << " | тип string, размер 24-32 б." << endl;
+	string s_3 = "Auwfiderzein"; cout << "s_1 = " << s_1 << " | тип string, размер 24-32 б." << endl;
+	string s_4 = "blackpencil"; cout << "s_1 = " << s_1 << " | тип string, размер 24-32 б." << endl;
+	string s_5 = "Nutbigbob"; cout << "s_1 = " << s_1 << " | тип string, размер 24-32 б." << endl;
+			
+	int _;
+	cin >> _;
+
+	return 0; 
+}
